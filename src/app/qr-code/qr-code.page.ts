655258742f9754949f7104b1eaad3c7283351705
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-12-22 13:39:30 +0800

    finish modal

---
+++ src/app/qr-code/qr-code.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-qr-code',
+  templateUrl: './qr-code.page.html',
+  styleUrls: ['./qr-code.page.scss'],
+})
+export class QrCodePage implements OnInit {
+  constructor(public modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  async dismiss() {
+    await this.modalController.dismiss();
+  }
+}
